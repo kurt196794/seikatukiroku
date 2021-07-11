a28890fee57e87ba50e8316cd9d06391b3274cf3
kurt196794 <kenta.kume1988@gmail.com>
2021-07-12 04:24:04 +0900

    userモデル、テスト実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,17 +6,64 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
-      user = FactoryBot.build(:user)  
-      user.name = '' 
-      user.valid?
-      expect(user.errors.full_messages).to include("Name can't be blank")
+    it 'ニックネームが必須であること' do
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name can't be blank")
     end
-    it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.email = ''
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+    it 'メールアドレスが必須であること' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
+    it 'メールアドレスが一意性であること' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+    it 'パスワードが必須であること' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'パスワードは、6文字以上での入力が必須であること（6文字が入力されていれば、登録が可能なこと）' do
+      @user.password = '111aa'
+      @user.password_confirmation = '111aa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+
+    it 'パスワードは、半角英数字混合での入力が必須であること（半角英数字が混合されていれば、登録が可能なこと）' do
+      @user.password = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password is invalid")
+    end
+
+    it 'パスワードは、半角英数字混合での入力が必須であること（半角英数字が混合されていれば、登録が可能なこと）' do
+      @user.password = '111111'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password is invalid")
+    end
+
+
+    it 'パスワードは、確認用を含めて2回入力すること' do
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'パスワードとパスワード（確認用）は、値の一致が必須であること' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it  'メールアドレスは、@を含む必要があること' do
+     @user.email = 'aa'
+     @user.valid?
+     expect(@user.errors.full_messages).to include("Email is invalid")
+   end
   end
 end
