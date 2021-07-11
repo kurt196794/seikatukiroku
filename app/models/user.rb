338cc016539ea69c99d202e6809cb5a7f6639244
kurt196794 <kenta.kume1988@gmail.com>
2021-07-12 03:22:35 +0900

    test導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
          validates :name, presence: true
+         validates :email, presence: true
 
-         
          has_many :records
 end
