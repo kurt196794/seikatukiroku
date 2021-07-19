04c168caa74a7b457e69e22a11660cecf6bd4b13
kurt196794 <kenta.kume1988@gmail.com>
2021-07-20 02:21:21 +0900

    db 前データ書き換え

--- db/schema.rb
+++ db/schema.rb
@@ -12,33 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_07_05_130616) do
 
-  create_table "kirokus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "name", null: false
-    t.string "room", null: false
-    t.integer "mealasa_id"
-    t.integer "mealhiru_id"
-    t.integer "mealyuu_id"
-    t.string "hydrationasa"
-    t.string "hydrationhiru"
-    t.string "hydrationyuu"
-    t.integer "excretionasa_id"
-    t.integer "excretionhiru_id"
-    t.integer "excretionyuu_id"
-    t.string "vitalasa"
-    t.string "vitalhiru"
-    t.string "vitalyuu"
-    t.integer "bathe_id"
-    t.integer "nightrecord_id"
-    t.text "diary"
-    t.text "nightdiary"
-    t.text "accident"
-    t.text "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_kirokus_on_user_id"
-  end
-
   create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.string "name", null: false
@@ -79,6 +52,5 @@ ActiveRecord::Schema.define(version: 2021_07_05_130616) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "kirokus", "users"
   add_foreign_key "records", "users"
 end
